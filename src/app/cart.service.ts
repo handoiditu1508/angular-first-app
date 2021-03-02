1ca2341ca59cd46b691080b8d5dcd00d42a0bf89
handoiditu1508 <handoiditu1508@gmail.com>
2021-03-02 20:42:13 +0700

    20:41 2021-03-02

---
+++ src/app/cart.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { products } from './products';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class CartService {
+	items = [];
+
+	constructor(private http: HttpClient) { }
+
+	addToCart(product) {
+		this.items.push(product);
+	}
+
+	getItems() {
+		return this.items;
+	}
+
+	clearCart() {
+		this.items = [];
+		return this.items;
+	}
+
+	getShippingPrices() {
+		return this.http.get("/assets/shipping.json");
+	}
+}
